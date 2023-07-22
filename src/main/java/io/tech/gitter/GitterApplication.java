266e4336d3cfaaec08576b32990eaf6170f3a886
VMWIN10 <vmwin10162@gmail.com>
2023-07-22 16:17:20 +0530

    Mod Java File

--- src/main/java/io/tech/gitter/GitterApplication.java
+++ src/main/java/io/tech/gitter/GitterApplication.java
@@ -1,14 +1,1 @@
-package io.tech.gitter;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GitterApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GitterApplication.class, args);
-		System.out.println("Hello Worldd!!");
-	}
-
-}
+EDITED
